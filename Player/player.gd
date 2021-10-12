4260460a2b10ffb23c4a7fa2e47097d16c3f99ed
fractiwi <64674935+kiwijuice56@users.noreply.github.com>
2021-10-11 22:48:25 -0500

    Added basic jumping/falling animation
    Used StateMachine within AnimationTree for more advanced animation combinations later on

--- Player/player.gd
+++ Player/player.gd
@@ -10,12 +10,12 @@ export var fall_accel := 0.6
 export var turn_speed := 0.05
 var x_speed := 0.0
 var y_speed := 0.0
-var dir := Vector3()
+var dir := Vector3(0,-1,0)
 
 onready var body = $Collision/KinematicBody
 
 func get_dir():
-	dir = Vector3(0, -1, 0)
+	dir = Vector3(0, dir.y, 0)
 	if Input.is_action_pressed("ui_left"):
 		dir.x -= 1
 	if Input.is_action_pressed("ui_right"):
@@ -35,7 +35,7 @@ func set_current_speed():
 		y_speed = 0
 	
 	if dir.y == 1.0:
-		y_speed = max(max_jump_speed, y_speed + jump_accel)
+		y_speed = min(max_jump_speed, y_speed + jump_accel)
 	elif dir.y == -1.0:
 		y_speed = max(-max_fall_speed, y_speed - fall_accel)
 	
@@ -44,12 +44,25 @@ func set_current_speed():
 		dir.y = -1
 
 func animate():
+	print(dir)
 	# rotates skeleton to dir
 	if not dir.x == 0:
 		$Tween.stop_all()
 		$Tween.interpolate_property($PolliwogKing/Skeleton, "rotation_degrees:y", $PolliwogKing/Skeleton.rotation_degrees.y, (90*dir.x)-90 , turn_speed, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 		$Tween.start()
-	$AnimationTree.set("parameters/RunIdle/blend_amount", abs(x_speed))
+	if dir.y == 1.0:
+		$AnimationTree.set("parameters/StateMachine/conditions/falling", false)
+		$AnimationTree.set("parameters/StateMachine/conditions/grounded", false)
+		$AnimationTree.set("parameters/StateMachine/conditions/jumping", true)
+	elif dir.y == -1.0:
+		$AnimationTree.set("parameters/StateMachine/conditions/jumping", false)
+		$AnimationTree.set("parameters/StateMachine/conditions/grounded", false)
+		$AnimationTree.set("parameters/StateMachine/conditions/falling", true)
+	if body.is_on_floor():
+		$AnimationTree.set("parameters/StateMachine/conditions/jumping", false)
+		$AnimationTree.set("parameters/StateMachine/conditions/falling", false)
+		$AnimationTree.set("parameters/StateMachine/conditions/grounded", true)
+	$AnimationTree.set("parameters/StateMachine/BlendTree/x_speed/blend_amount", abs(x_speed))
 
 func _physics_process(delta: float):
 	body.move_and_slide(Vector3(max_run_speed * x_speed, y_speed, 0), Vector3.UP)
